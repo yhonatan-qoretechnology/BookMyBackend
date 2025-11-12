6eea5b5b73085429bf34ae81491d6208239d144d
yhonatanback <yhonatanback@galaxypay.app>
2025-11-11 23:55:51 -0500

    fix:agenar cita

--- src/data/appointment/dto/create-appointment.dto.ts
+++ src/data/appointment/dto/create-appointment.dto.ts
@@ -6,6 +6,7 @@ import {
   IsInt,
   IsOptional,
   IsString,
+  Min,
 } from 'class-validator';
 
 export class CreateAppointmentDto {
@@ -32,6 +33,7 @@ export class CreateAppointmentDto {
 
   @ApiProperty({ example: 30, description: 'Duración del servicio en minutos' })
   @IsInt()
+  @Min(1)
   duracion: number;
 
   @ApiProperty({ enum: AppointmentStatus, example: AppointmentStatus.PENDING })
