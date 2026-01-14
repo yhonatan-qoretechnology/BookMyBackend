bb12e00cc33dd9854cf2c78e1b41cd12f929e6ee
yhonatanback <yhonatanback@galaxypay.app>
2026-01-13 21:08:46 -0500

    fix: correcionde configuraciones

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -2,15 +2,5 @@ import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  earlyAccess: true,
   schema: 'prisma/schema.prisma',
-  migrate: {
-    adapter: async () => {
-      const { PrismaPg } = await import('@prisma/adapter-pg');
-      const { Pool } = await import('pg');
-
-      const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-      return new PrismaPg(pool);
-    },
-  },
 });
