69b98e6be768a8d63f6ab309df6d4d8485bb0df9
yhonatanback <yhonatanback@galaxypay.app>
2026-03-27 14:08:00 -0500

    fix: otp corrreo

--- src/data/otp/otp.service.ts
+++ src/data/otp/otp.service.ts
@@ -3,7 +3,7 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import dayjs from 'dayjs';
+import * as dayjs from 'dayjs';
 import { PrismaService } from '../../prisma/prisma.service';
 import { MailService } from '../email/mail.service';
 import { SmsService } from '../sms/sms.service';
